30ecb568c120a61d5b0606b243077976e2e3066f
Dotoli <zzphoodev@gmail.com>
2022-09-10 13:42:31 +0900

    Add: Post.page building CSS

--- src/Router.js
+++ src/Router.js
@@ -1,8 +1,9 @@
 import React from "react";
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Post from "./pages/Post";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/new" element={<Register />} />
+        <Route path="/post" element={<Post />} />
       </Routes>
     </BrowserRouter>
   );
