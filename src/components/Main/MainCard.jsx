c3f42ba8b8ffb86b9a59c4e6b7e21614296d6d66
Hwang-Junsu <hjs77728@gmail.com>
2022-09-10 18:20:30 +0900

    Letter Error

---
+++ src/components/Main/MainCard.jsx
@@ -0,0 +1,245 @@
+import React, {useState} from "react";
+import styled from "styled-components";
+const MainCard = () => {
+  const [like, setLike] = useState(false);
+
+  return (
+    <>
+      <Div className="col-sm-4 col-xs-12">
+        <Card class="card">
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://i.pinimg.com/564x/29/f6/df/29f6dfff21b5e71169245e389ced72bd.jpg"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content">
+              <h3 style={{margin: "2px"}}>soOoO0ng</h3>
+              <h>@soonger</h>
+            </div>
+          </Media>
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <Img
+                src="https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/166266591662644543.jpeg?gif=1&w=1080&webp=1"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content"></div>
+          </Media>
+        </Card>
+        <Card class="card">
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content">
+              <h3 style={{margin: "2px"}}>soOoO0ng</h3>
+              <h>@soonger</h>
+            </div>
+          </Media>
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <Img
+                src="https://i.pinimg.com/564x/d5/a1/3f/d5a13f762ae81a53be6c2557500d5465.jpg"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content"></div>
+          </Media>
+        </Card>
+        <Card class="card">
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://i.pinimg.com/564x/29/f6/df/29f6dfff21b5e71169245e389ced72bd.jpg"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content">
+              <h3 style={{margin: "2px"}}>soOoO0ng</h3>
+              <h>@soonger</h>
+            </div>
+          </Media>
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <Img
+                src="https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/166266591662644543.jpeg?gif=1&w=1080&webp=1"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content"></div>
+          </Media>
+        </Card>
+        <Card class="card">
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://i.pinimg.com/564x/29/f6/df/29f6dfff21b5e71169245e389ced72bd.jpg"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content">
+              <h3 style={{margin: "2px"}}>soOoO0ng</h3>
+              <h>@soonger</h>
+            </div>
+          </Media>
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <Img
+                src="https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/166266591662644543.jpeg?gif=1&w=1080&webp=1"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content"></div>
+          </Media>
+        </Card>
+        <Card class="card">
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://i.pinimg.com/564x/29/f6/df/29f6dfff21b5e71169245e389ced72bd.jpg"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content">
+              <h3 style={{margin: "2px"}}>soOoO0ng</h3>
+              <h>@soonger</h>
+            </div>
+          </Media>
+
+          <div class="card-image">
+            <figure class="image is-4by3">
+              <Img
+                src="https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/snapshots/166266591662644543.jpeg?gif=1&w=1080&webp=1"
+                alt="Placeholder image"
+              />
+            </figure>
+          </div>
+          <Media class="media">
+            <div class="media-left">
+              <img
+                src="https://bulma.io/images/placeholders/96x96.png"
+                alt="Placeholder image"
+                style={{
+                  width: "50px",
+                  height: "50px",
+                  borderRadius: "30px",
+                }}
+              />
+            </div>
+            <div class="media-content"></div>
+          </Media>
+        </Card>
+      </Div>
+    </>
+  );
+};
+
+export default MainCard;
+
+const Media = styled.div`
+  display: flex;
+  margin-left: 40px;
+`;
+const Img = styled.img`
+  width: 300px;
+  height: 400px;
+  background-size: cover;
+`;
+const Div = styled.div`
+  display: flex;
+  align-content: flex-start;
+  flex-wrap: wrap;
+  width: 97%;
+  margin: 0;
+`;
+
+const Card = styled.div`
+  margin-bottom: 40px;
+`;
