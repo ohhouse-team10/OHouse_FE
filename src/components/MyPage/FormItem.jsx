f27cc1d1a6945fca7ca0f361284904ba04774bab
Hwang-Junsu <hjs77728@gmail.com>
2022-09-10 20:28:35 +0900

    [ADD] Mypage

---
+++ src/components/MyPage/FormItem.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import styled from "styled-components";
+
+const FormItem = ({title, required = false, isImg = false, msg = ""}) => {
+  return (
+    <Container>
+      <FormItemTitle>
+        {title}
+        {required ? <div>{"* 필수항목"}</div> : null}
+      </FormItemTitle>
+      <FormItemContent>
+        <FormItemInputBox>
+          {isImg ? (
+            <FormImgBox>
+              <FormImg
+                src={
+                  "https://image.ohou.se/i/bucketplace-v2-development/uploads/users/profile_images/1662687088_kakao_2422245639.jpg?gif=1&w=640&h=640&c=c&webp=1"
+                }
+              />
+            </FormImgBox>
+          ) : (
+            <FormItemInput />
+          )}
+        </FormItemInputBox>
+        <Msg>{msg}</Msg>
+      </FormItemContent>
+    </Container>
+  );
+};
+
+export default FormItem;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const FormItemTitle = styled.div`
+  padding-top: 45px;
+  width: 100px;
+  letter-spacing: -2px;
+  margin-right: 10px;
+
+  div {
+    margin-top: 5px;
+    font-size: 13px;
+    color: #757575;
+  }
+`;
+
+const FormItemContent = styled.div`
+  max-width: 400px;
+  flex: 1 0 0px;
+  padding: 20px 0;
+`;
+
+const FormItemInputBox = styled.div`
+  padding: 20px 0;
+`;
+
+const FormItemInput = styled.input`
+  text-align: left;
+  font-size: 15px;
+  border-radius: 6px;
+  transition: border-color 0.2s, box-shadow 0.2s, background-color 0.2s;
+  display: block;
+  box-sizing: border-box;
+  height: 40px;
+  width: 100%;
+  padding: 0 15px;
+  line-height: 40px;
+  border: 1px solid #dbdbdb;
+  background-color: #fff;
+  color: #424242;
+`;
+
+const FormImgBox = styled.div`
+  width: 200px;
+  height: 200px;
+`;
+const FormImg = styled.img`
+  width: 200px;
+  height: 200px;
+`;
+
+const Msg = styled.div`
+  font-size: 12px;
+  font-weight: 700;
+  margin-top: -15px;
+`;
--- src/components/MyPage/MyPageNav.jsx
+++ src/components/MyPage/MyPageNav.jsx
@@ -2,7 +2,88 @@ import React from "react";
 import styled from "styled-components";
 
 const MyPageNav = () => {
-  return <div>MyPageNav</div>;
+  return (
+    <NavContainer>
+      <NavContents fontSize={18}>
+        <ul>
+          <li>
+            <a>프로필</a>
+          </li>
+          <li>
+            <a>나의 쇼핑</a>
+          </li>
+          <li>
+            <a>나의 리뷰</a>
+          </li>
+          <li>
+            <a>설정</a>
+          </li>
+        </ul>
+      </NavContents>
+      <NavContents fontSize={15}>
+        <ul>
+          <li>
+            <a>회원정보수정</a>
+          </li>
+          <li>
+            <a>알림 설정</a>
+          </li>
+          <li>
+            <a>사용자 숨기기 설정</a>
+          </li>
+          <li>
+            <a>전문가 신청</a>
+          </li>
+          <li>
+            <a>비밀번호 변경</a>
+          </li>
+          <li>
+            <a>추천코드</a>
+          </li>
+        </ul>
+      </NavContents>
+    </NavContainer>
+  );
 };
 
 export default MyPageNav;
+
+const NavContainer = styled.div`
+  position: relative;
+  width: 100%;
+  margin: 0 auto;
+`;
+
+const NavContents = styled.div`
+  font-size: ${(props) => props.fontSize}px;
+  overflow: hidden;
+  border-bottom: 1px solid #ededed;
+  width: 100%;
+
+  ul {
+    text-align: center;
+    white-space: nowrap;
+    list-style: none;
+
+    li {
+      display: inline-block;
+
+      a {
+        &:not(.active):hover {
+          color: #35c5f0;
+        }
+
+        display: block;
+        padding: 0 10px;
+        font-weight: 700;
+        position: relative;
+        height: 60px;
+        line-height: 60px;
+        transition: color 0.15s ease;
+
+        cursor: pointer;
+        text-decoration: none;
+      }
+    }
+  }
+`;
