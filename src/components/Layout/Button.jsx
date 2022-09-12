84ae3be54f21c0ca9cb7cafd1a6aeeb15cb039dd
Hwang-Junsu <hjs77728@gmail.com>
2022-09-12 22:07:24 +0900

    [Fix] Button

--- src/components/Layout/Button.jsx
+++ src/components/Layout/Button.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 
+/** 폰트사이즈는 18, 패딩은 15px 10px가 기본입니다.
+ * btnName : string -> 버튼 내부의 이름을 설정할 수 있습니다.
+ * disabled : boolean -> 버튼 활성 비활성을 설정할 수 있습니다. true => 비활성, false => 활성
+ */
+
 const Button = ({fontSize = 18, btnName, disabled, padding = "15px 10px"}) => {
   return (
     <Btn disabled={disabled} padding={padding} fontSize={fontSize}>
