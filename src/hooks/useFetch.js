a6591016440268b152cbd36d9e591c464ae96d7c
soonger3306 <soonger3306@daum.net>
2022-09-13 20:05:52 +0900

    0913

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import {useDispatch,useSelector} from "react-redux"
+import {getInfiniteList} from "../redux/modules/PostSlice";
+
+const END_POINT = 'https://';
+const useFetch = (page) => {
+//list => 리덕스,post 로 사용해서 안씀 
+//   const [list, setList] = useState([]);
+
+  const [hasMore, setHasMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+
+  //query API 요청 보내기
+  const sendQuery = useCallback(async () => {
+   
+    try {
+      
+      let response = await dispatch(getInfiniteList(page));
+      response = response.payload
+      
+   
+    //   setList((prev) => [...new Set([...prev, ...response])]);
+      setHasMore(response.length > 0);
+      setIsLoading(false);
+    } catch (e) {
+      throw new Error(`오류입니다. ${e.message}`);
+    }
+  }, [page]);
+
+  useEffect(() => {
+ if (!isLoading)
+  {sendQuery()
+ setIsLoading(true)}
+
+  }, [sendQuery, page]);
+
+  return { hasMore,
+    // list,
+    isLoading };
+};
+export default useFetch;
